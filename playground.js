71e44f77f0713dba9ca30adf81ad3512f9359f31
Mikalai19 <bertman29@gmail.com>
2022-01-05 13:34:38 -0800

    import Comments and added data to the comments

--- playground.js
+++ playground.js
@@ -3,6 +3,7 @@ const { Sales } = require("./Models");
 const { Jobs } = require("./Models");
 const { Forums } = require("./Models")
 const { Posts } = require("./Models")
+const { Comments } = require("./Models");
 
 // create jobs
 db.jobs.insertMany([
@@ -28,3 +29,33 @@ db.jobs.insertMany([
         console.log(error);
     });
 
+//create a comments
+db.comments.insertMany([
+    {
+        header: 'Most Covid infections are fake and mass hysteria',
+        content: 'They believe a threat to be real because someone says so, </br>or because it fits their experience. Due to the threatening delusion, a large group of people gets collectively very upset. In other words, a threat, whether real or imaginary [17], causes collective anxiety [18].',
+        date: new Date()
+    },
+    {
+        header: "Let's never forget this gem",
+        content: "Look, the results of the vaccine are very good, and if you do get (Covid), it's a very minor form.",
+        date: new Date()
+    },
+    {
+        header: "I've solved the energy crisis",
+        content: 'Turn cellulite into fuel. Mandatory lipo for all fat asses in the USA',
+        date: new Date()
+    },
+    {
+        header: 'FYI Do not sign a solar contract w Sunnova',
+        content: "We purchased a house w an existing Sunnova contract.I didn't know it but the system was already not working",
+        date: new Date()
+    },
+    {
+        header: 'New Chef Ramsey show starts tonight',
+        content: 'Next Level Chef. Welcome to the Next Level. Fifteen contestants from around the country prepare to level up their cooking skills.',
+        date: new Date()
+    }
+
+
+]);
