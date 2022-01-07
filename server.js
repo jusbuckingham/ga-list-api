a241c671c0a031b482bf0dfe07c981ebdacaffca
jusbuckingham <jus.buckingham@gmail.com>
2022-01-07 14:25:52 -0800

    kyles stored backend command

--- server.js
+++ server.js
@@ -107,6 +107,14 @@ app.get("/comments", async (request, response) => {
     }
 });
 
+app.post('/stored', (req, res) => {
+    console.log(req.body);
+    db.collection('quotes').insertOne(req.body, (err, data) => {
+        if(err) return console.log(err);
+        res.send(('saved to db: ' + data));
+    })
+});
+
 //app.use('/users', users);
 
 //import controllers
