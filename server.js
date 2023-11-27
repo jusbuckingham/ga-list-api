7346bcdf06d54fa111df662a01c80b91d756de1c
jus <jus.buckingham@gmail.com>
2023-11-27 12:52:23 -0800

    refactor server.js

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 const express = require("express");
-const { User, Sales, Jobs, Forums, Posts, Comments } = require("./models");
-const app = express();
 const cors = require('cors');
 const passport = require('passport');
 require('dotenv').config();
+
+// Import Models
+const { User, Sales, Jobs, Forums, Posts, Comments } = require("./models");
+
+// Passport Config
 require('./config/passport')(passport);
 
+// Import Controllers
+const usersController = require('./controllers/users');
+const forumsController = require('./controllers/forums');
+const jobsController = require('./controllers/jobs');
+const postsController = require('./controllers/posts');
+const salesController = require('./controllers/sales');
+const commentsController = require('./controllers/comments');
+
+const app = express();
+
+// Middlewares
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+app.use(passport.initialize());
+
+// CORS Headers
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.header(
@@ -15,118 +35,23 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(cors());
-app.use(passport.initialize());
+// Routes
+app.get('/', (req, res) => res.json({ message: "Welcome to the MERN Auth API" }));
 
-// controllers
-const users = require('./controllers/users');
-const { request, response } = require("express");
-const res = require("express/lib/response");
+// Route Handlers
+app.use('/users', usersController);
+app.use('/forums', forumsController);
+app.use('/jobs', jobsController);
+app.use('/posts', postsController);
+app.use('/sales', salesController);
+app.use('/comments', commentsController);
 
-const forums = require('./controllers/forums');
-const jobs = require('./controllers/jobs');
-const posts = require('./controllers/posts');
-const sales = require('./controllers/sales');
-
-
-// routes
-app.get('/', (req, res) => {
-    res.json({
-        message: "Welcome to the MERN Auth API"
-    });
+// Error Handling
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
 });
 
-app.get("/users", async (request, response) => {
-
-    try {
-        const usersArray = await Users.find({});
-        response.json({ usersArray });
-    } catch (error) {
-        response.status(500).send(error);
-    }
-});
-
-app.get("/sales", async (request, response) => {
-
-    try {
-        const salesArray = await Sales.find({});
-        response.json({ salesArray });
-    } catch (error) {
-        response.status(500).send(error);
-    }
-
-});
-
-// app.use(express.json());
-
-app.get("/jobs", async (request, response) => {
-
-    try {
-        const jobsArray = await Jobs.find({})
-        response.json({ jobsArray });
-    } catch (error) {
-        response.status(500).send(error);
-    }
-})
-
-app.get("/forums", async (request, response) => {
-
-    try {
-        const forumsArray = await Forums.find({});
-        response.json({ forumsArray });
-    } catch (error) {
-        response.status(500).send(error);
-    }
-});
-
-app.get("/posts", async (request, response) => {
-
-    try {
-        const postsArray = await Posts.find({});
-        response.json({ postsArray });
-    } catch (error) {
-        response.status(500).send(error);
-    }
-});
-
-
-app.get("/comments", async (request, response) => {
-    try {
-        const commentsArray = await Comments.find({});
-        response.json({ commentsArray });
-
-    } catch (error) {
-        response.status(500).send(error);
-    }
-});
-
-app.post('/stored', (req, res) => {
-    console.log(req.body);
-    db.collection('quotes').insertOne(req.body, (err, data) => {
-        if (err) return console.log(err);
-        res.send(('saved to db: ' + data));
-    })
-});
-
-
-
-//import controllers
-app.use('/users', require('./controllers/users'));
-app.use('/sales', require('./controllers/sales'));
-app.use('/posts', require('./controllers/posts'));
-app.use('/jobs', require('./controllers/jobs'));
-app.use('/forums', require('./controllers/forums'));
-app.use('/comments', require('./controllers/comments'));
-
-
-app.use('/users', users);
-app.use('/forums', forums);
-app.use('/jobs', jobs);
-app.use('/posts', posts);
-app.use('/sales', users);
-
-app.listen(process.env.PORT || 3000, () => {
-    console.log("Server is jammin' on port 3000 🎧")
-});
+// Start Server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is jammin' on port ${PORT} 🎧`));
